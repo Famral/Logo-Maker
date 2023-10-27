be8d524c388720036d9971cd881595f6bb75b82a
Manish Meena <108734050+ConnectWithMan@users.noreply.github.com>
2023-10-27 16:05:11 +0530

    Add files via upload

---
+++ asset/_next/static/VqmAEW8HlpcjskqKWjCRs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = { __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] }, "/": ["static/chunks/464-477e76c56d8bcb47.js", "static/css/8465c012012f8b88.css", "static/chunks/pages/index-9883d1426a428731.js"], "/_error": ["static/chunks/pages/_error-8353112a01355ec2.js"], sortedPages: ["/", "/_app", "/_error"] }, self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
